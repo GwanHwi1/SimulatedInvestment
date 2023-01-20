b0436a0a484e00305f48bc006fbdb65c9b6aa6c5
GwanHwi1 <ghc98@naver.com>
2023-01-21 00:02:41 +0900

    start

---
+++ src/main/java/com/investment/simulatedInvestment/SimulatedInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.investment.simulatedInvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatedInvestmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/investment/simulatedInvestment/SimulatedInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment.simulatedInvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatedInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
