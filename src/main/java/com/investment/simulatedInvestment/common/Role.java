3c3cffa5d17a6ff97c6018432ce73d56cccdf921
박준용 <jo101044@naver.com>
2023-01-21 13:28:37 +0900

    feat : security basic setting

--- src/main/java/com/investment/simulatedInvestment/common/Role.java
+++ src/main/java/com/investment/simulatedInvestment/common/Role.java
@@ -1,5 +1,16 @@
 package com.investment.simulatedInvestment.common;
 
 public enum Role {
-    USER,ADMIN
+    USER("user"),ADMIN("admin");
+
+    private final String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return role;
+    }
 }
