45fc02a446d53198d01ac7cfee8135b7acfeac9b
GwanHwi1 <ghc98@naver.com>
2023-01-25 18:12:54 +0900

    Error: 회원가입 이슈

--- src/main/java/com/investment/simulatedInvestment/config/security/CustomUserDetails.java
+++ src/main/java/com/investment/simulatedInvestment/config/security/CustomUserDetails.java
@@ -1,74 +1,74 @@
-package com.investment.simulatedInvestment.config.security;
-
-import com.investment.simulatedInvestment.dto.MemberDto;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-
-public class CustomUserDetails implements UserDetails, OAuth2User {
-    private final MemberDto memberDto;
-    private Map<String, Object> attributes;
-
-    // 일반 로그인
-    public CustomUserDetails(MemberDto memberDto) {
-        this.memberDto = memberDto;
-    }
-
-    // Oauth 로그인
-    public CustomUserDetails(MemberDto memberDto, Map<String, Object> attributes) {
-        this.memberDto = memberDto;
-        this.attributes = attributes;
-    }
-
-    @Override
-    public String getName() {
-        return memberDto.getNickname();
-    }
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return attributes;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> role = new ArrayList<>();
-        role.add((GrantedAuthority) () -> memberDto.getRole().toString());
-
-        return role;
-    }
-
-    @Override
-    public String getPassword() {
-        return memberDto.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return memberDto.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}
+//package com.investment.simulatedInvestment.config.security;
+//
+//import com.investment.simulatedInvestment.dto.MemberDto;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.Map;
+//
+//public class CustomUserDetails implements UserDetails, OAuth2User {
+//    private final MemberDto memberDto;
+//    private Map<String, Object> attributes;
+//
+//    // 일반 로그인
+//    public CustomUserDetails(MemberDto memberDto) {
+//        this.memberDto = memberDto;
+//    }
+//
+//    // Oauth 로그인
+//    public CustomUserDetails(MemberDto memberDto, Map<String, Object> attributes) {
+//        this.memberDto = memberDto;
+//        this.attributes = attributes;
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return memberDto.getNickname();
+//    }
+//
+//    @Override
+//    public Map<String, Object> getAttributes() {
+//        return attributes;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        Collection<GrantedAuthority> role = new ArrayList<>();
+//        role.add((GrantedAuthority) () -> memberDto.getRole().toString());
+//
+//        return role;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return memberDto.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return memberDto.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
--- src/main/java/com/investment/simulatedInvestment/service/MemberService.java
+++ src/main/java/com/investment/simulatedInvestment/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.investment.simulatedInvestment.service;
 
+import com.investment.simulatedInvestment.common.Role;
 import com.investment.simulatedInvestment.dto.MemberDto;
 import com.investment.simulatedInvestment.entity.Member;
 import com.investment.simulatedInvestment.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public Long createUser(MemberDto dto){
