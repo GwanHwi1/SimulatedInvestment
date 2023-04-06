82a96745628bcdea92879a89c3c618d288d36a11
GwanHwi1 <ghc98@naver.com>
2023-04-07 05:41:23 +0900

    CustomAccessDeniedHandler 코드 수정

--- src/main/java/com/investment/simulatedInvestment/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/investment/simulatedInvestment/handler/CustomAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package com.investment.simulatedInvestment.handler;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -21,8 +22,11 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
                        AccessDeniedException e
     ) throws IOException, ServletException {
 
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        request.getRequestDispatcher("/errorPage").forward(request, response);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write("<script>alert('권한이 없습니다.'); location.href='/';</script>");
+
+//        response.setStatus(HttpStatus.FORBIDDEN.value());
+//        request.getRequestDispatcher("/errorPage").forward(request, response);
     }
 
 }
